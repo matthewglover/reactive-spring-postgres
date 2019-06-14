126a04c341d4bc962167d524d791cfd9bf734a99
Matt Glover <mglover@8thlight.com>
2019-06-14 17:03:50 +0100

    Add EnableConfigurationProperties in PostgresConfig

--- src/main/kotlin/com/matthewglover/reactivepostgres/postgres/PostgresConfig.kt
+++ src/main/kotlin/com/matthewglover/reactivepostgres/postgres/PostgresConfig.kt
@@ -3,11 +3,13 @@ package com.matthewglover.reactivepostgres.postgres
 import com.matthewglover.reactivepostgres.util.StringUtil
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration
 import io.r2dbc.postgresql.PostgresqlConnectionFactory
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.r2dbc.function.DatabaseClient
 
 @Configuration
+@EnableConfigurationProperties(DataSourceConfig::class)
 class PostgresConfig {
     @Bean
     fun databaseClient(dataSourceConfig: DataSourceConfig): DatabaseClient {
